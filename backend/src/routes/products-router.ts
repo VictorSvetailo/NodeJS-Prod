8b986d251dab03e9e683fff528a29cbfa2091713
VictorSvetailo <victorioeve@gmail.com>
2022-12-12 17:13:53 +0300

    feat(division into layers) created the repositories layer

--- backend/src/routes/products-router.ts
+++ backend/src/routes/products-router.ts
@@ -1,20 +1,15 @@
 import {Request, Response, Router} from 'express';
 
-const products = [{id: 1, title: 'tomato'}, {id: 2, title: 'orange'}]
 
 export const productsRouter = Router({})
 
 //get
 productsRouter.get('/', (req: Request, res: Response) => {
-    if (req.query.title) {
-        let searchString = req.query.title.toString();
-        res.send(products.filter(p => p.title.indexOf(searchString) > -1))
-    } else {
-        res.send(products)
-    }
+    const foundProducts = productsRepository.findProducts(req.query.title?.toString())
+    res.send(foundProducts)
 })
 productsRouter.get('/:id', (req: Request, res: Response) => {
-    let product = products.find(p => p.id == +req.params.id)
+    const product = productsRepository.findProductsById(+req.params.id)
     if (product) {
         res.send(product)
     } else {
@@ -23,31 +18,26 @@ productsRouter.get('/:id', (req: Request, res: Response) => {
 })
 //post
 productsRouter.post('/', (req: Request, res: Response) => {
-    const newProduct = {
-        id: +(new Date()),
-        title: req.body.title
-    }
-    products.push(newProduct)
+    const newProduct = productsRepository.createProduct(req.body.title)
     res.status(201).send(newProduct)
 })
 //put
 productsRouter.put('/:id', (req: Request, res: Response) => {
-    let product = products.find(p => p.id == +req.params.id)
-    if (product) {
-        product.title = req.body.title
+    const isUpdated = productsRepository.updateProduct(+req.params.id, req.body.title)
+    if (isUpdated) {
+        const product = productsRepository.findProductsById(+req.params.id)
         res.send(product)
     } else {
-        res.send(200)
+        res.send(404)
     }
 })
 //delete
 productsRouter.delete('/:id', (req: Request, res: Response) => {
-    for (let i = 0; i < products.length; i++) {
-        if (products[i].id === +req.params.id) {
-            products.splice(i, 1);
-            res.send(204)
-            return
-        }
+    const isDeleted = productsRepository.deleteProduct(+req.params.id)
+    if (isDeleted) {
+        res.send(204)
+    } else {
+        res.send(404)
     }
-    res.send(404)
+
 })
