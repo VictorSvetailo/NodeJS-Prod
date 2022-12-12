3074866afddd93280b48cf45c6f142e1955c80aa
VictorSvetailo <victorioeve@gmail.com>
2022-12-12 19:08:08 +0300

    feat(validation) input validation, express-validator

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import express, {NextFunction, Request, Response} from 'express'
 import bodyParser from 'body-parser';
+import {addressesRouter} from './routes/addresses-router';
+import {productsRouter} from './routes/products-router';
 
 const app = express()
 
@@ -7,53 +9,57 @@ const parserMiddleware = bodyParser({})
 app.use(parserMiddleware)
 
 
-const testMiddleware = (req: Response, res: Request, next: NextFunction) => {
-    //@ts-ignore
-    req.test = 'Hello i am Test'
-    next()
-}
-
-const authGardMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    if (req.query.token === '123') {
-        next()
-    } else {
-        res.send(401)
-    }
-}
-
-let requestCounter = 0
-
-const requestCounterMiddleware = (req: Response, res: Request, next: NextFunction) => {
-    requestCounter++
-    next()
-}
-
-app.use(requestCounterMiddleware)
-app.use(testMiddleware)
-app.use(authGardMiddleware)
-
 
 const port = process.env.PORT || 3000
-
-
-app.get('/products', (req: Request , res: Response) => {
-    //@ts-ignore
-    const test = req.test
-    res.send({value: test + 'from users???' + requestCounter})
-})
-
-
-app.get('/users', (req: Request , res: Response) => {
-    //@ts-ignore
-    const test = req.test
-    res.send({value: test + 'from users???' + requestCounter})
-})
-
-
-// app.use('/products', productsRouter)
-// app.use('/addresses', addressesRouter)
+app.use('/products', productsRouter)
+app.use('/addresses', addressesRouter)
 
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+
+// const testMiddleware = (req: Response, res: Request, next: NextFunction) => {
+//     //@ts-ignore
+//     req.test = 'Hello i am Test'
+//     next()
+// }
+//
+// const authGardMiddleware = (req: Request, res: Response, next: NextFunction) => {
+//     if (req.query.token === '123') {
+//         next()
+//     } else {
+//         res.send(401)
+//     }
+// }
+//
+// let requestCounter = 0
+//
+// const requestCounterMiddleware = (req: Response, res: Request, next: NextFunction) => {
+//     requestCounter++
+//     next()
+// }
+// //@ts-ignore
+// app.use(requestCounterMiddleware)
+// //@ts-ignore
+// app.use(testMiddleware)
+// app.use(authGardMiddleware)
+//
+//
+
+
+//
+// app.get('/products', (req: Request , res: Response) => {
+//     //@ts-ignore
+//     const test = req.test
+//     res.send({value: test + 'from users???' + requestCounter})
+// })
+//
+//
+// app.get('/users', (req: Request , res: Response) => {
+//     //@ts-ignore
+//     const test = req.test
+//     res.send({value: test + 'from users???' + requestCounter})
+// })
--- backend/src/routes/products-router.ts
+++ backend/src/routes/products-router.ts
@@ -1,8 +1,19 @@
 import {Request, Response, Router} from 'express';
 import {productsRepository} from '../repositories/products-repository';
+import {body, validationResult} from 'express-validator';
+import {inputValidationMiddleware} from '../midlewares/input-validation-middleware';
 
 
 export const productsRouter = Router({})
+
+
+const titleValidation = body('title').trim().isLength({
+    min: 1,
+    max: 10
+}).withMessage('Title length should be from 3 to 10 symbols')
+
+
+
 
 //get
 productsRouter.get('/', (req: Request, res: Response) => {
@@ -18,20 +29,27 @@ productsRouter.get('/:id', (req: Request, res: Response) => {
     }
 })
 //post
-productsRouter.post('/', (req: Request, res: Response) => {
-    const newProduct = productsRepository.createProduct(req.body.title)
-    res.status(201).send(newProduct)
-})
+productsRouter.post('/',
+    titleValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
+        const newProduct = productsRepository.createProduct(req.body.title)
+        res.status(201).send(newProduct)
+    })
 //put
-productsRouter.put('/:id', (req: Request, res: Response) => {
-    const isUpdated = productsRepository.updateProduct(+req.params.id, req.body.title)
-    if (isUpdated) {
-        const product = productsRepository.findProductsById(+req.params.id)
-        res.send(product)
-    } else {
-        res.send(404)
-    }
-})
+productsRouter.put('/:id',
+    titleValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
+
+        const isUpdated = productsRepository.updateProduct(+req.params.id, req.body.title)
+        if (isUpdated) {
+            const product = productsRepository.findProductsById(+req.params.id)
+            res.send(product)
+        } else {
+            res.send(404)
+        }
+    })
 //delete
 productsRouter.delete('/:id', (req: Request, res: Response) => {
     const isDeleted = productsRepository.deleteProduct(+req.params.id)
