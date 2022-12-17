72fcb5bc00d62d8eb966cbe192d660254a2ff63f
VictorSvetailo <victorioeve@gmail.com>
2022-12-17 13:03:04 +0300

    feat() vercel 2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,56 +11,19 @@ app.use(parserMiddleware)
 
 
 const port = process.env.PORT || 3000
-app.use('/', productsRouter)
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello Samurai')
+})
 app.use('/products', productsRouter)
 app.use('/addresses', addressesRouter)
 
 
+// const startApp = async () => {
+//     await runDb()
+//     app.listen(port, () => {
+//         console.log(`Example app listening on port ${port}`)
+//     })
+// }
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-
-
-
-// const testMiddleware = (req: Response, res: Request, next: NextFunction) => {
-//     //@ts-ignore
-//     req.test = 'Hello i am Test'
-//     next()
-// }
-//
-// const authGardMiddleware = (req: Request, res: Response, next: NextFunction) => {
-//     if (req.query.token === '123') {
-//         next()
-//     } else {
-//         res.send(401)
-//     }
-// }
-//
-// let requestCounter = 0
-//
-// const requestCounterMiddleware = (req: Response, res: Request, next: NextFunction) => {
-//     requestCounter++
-//     next()
-// }
-// //@ts-ignore
-// app.use(requestCounterMiddleware)
-// //@ts-ignore
-// app.use(testMiddleware)
-// app.use(authGardMiddleware)
-//
-//
-
-
-//
-// app.get('/products', (req: Request , res: Response) => {
-//     //@ts-ignore
-//     const test = req.test
-//     res.send({value: test + 'from users???' + requestCounter})
-// })
-//
-//
-// app.get('/users', (req: Request , res: Response) => {
-//     //@ts-ignore
-//     const test = req.test
-//     res.send({value: test + 'from users???' + requestCounter})
-// })
